7cd8da12125c970eed69b15b273c97046e171e0f
Sebastian Spaink <sspaink@influxdata.com>
2022-11-01 22:30:40 -0500

    fix: reduce cyclomatic complexity

--- modfile.go
+++ modfile.go
@@ -25,36 +25,44 @@ func Parse(data []byte) (*ModFile, error) {
 	var f ModFile
 
 	for scanner.Scan() {
-		switch s := scanner.Text(); s {
+		switch scanner.Text() {
 		case "require (":
 			require = true
+
+			continue
 		case ")":
 			require = false
-		default:
-			if !require {
-				continue
-			}
 
-			// If in a require section, parse the module
-			// A module is required to have a path and version, the "indirect" comment is optional
-			var mod Module
-			m := strings.Split(strings.TrimSpace(s), " ")
-			const pathAndVersion = 2
-			if len(m) == pathAndVersion {
-				mod.Path = m[0]
-				mod.Version = m[1]
-			}
-			var indirect bool
-			if len(m) == 4 && fmt.Sprintf("%s %s", m[2], m[3]) == "// indirect" {
-				indirect = true
-			}
-			if len(m) >= pathAndVersion {
-				if indirect {
-					f.Indirect = append(f.Indirect, &mod)
-				} else {
-					f.Direct = append(f.Direct, &mod)
-				}
-			}
+			continue
+		}
+
+		if !require {
+			continue
+		}
+
+		// If in a require section, parse the module
+		// A module is required to have a path and version, the "indirect" comment is optional
+		var mod Module
+		m := strings.Split(strings.TrimSpace(scanner.Text()), " ")
+		const pathAndVersion = 2
+		if len(m) == pathAndVersion {
+			mod.Path = m[0]
+			mod.Version = m[1]
+		}
+		// Skip if invalid module, missing required path or version
+		if len(m) < pathAndVersion {
+			continue
+		}
+
+		var indirect bool
+		if len(m) == 4 && fmt.Sprintf("%s %s", m[2], m[3]) == "// indirect" {
+			indirect = true
+		}
+
+		if indirect {
+			f.Indirect = append(f.Indirect, &mod)
+		} else {
+			f.Direct = append(f.Direct, &mod)
 		}
 	}
 
